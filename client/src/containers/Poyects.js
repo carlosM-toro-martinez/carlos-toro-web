af6ea3465d32a07e00a7f86ed4e7e2e9872a0181
Carlos Toro <carlostorom.96@gmail.com>
2023-03-16 16:15:18 -0400

    add: first  proyects

--- client/src/containers/Poyects.js
+++ client/src/containers/Poyects.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./proyects.css";
-import certificate1 from "../public/certificate1.png";
-import certificate2 from "../public/certificate2.png";
-import certificate3 from "../public/certificate3.png";
-import certificate4 from "../public/certificate4.png";
-import certificate5 from "../public/certificate5.png";
+import encuentra from "../public/proyects/encuentra.jpg";
+import arcane from "../public/proyects/arcane.png";
+import continentes from "../public/proyects/continentes.png";
+import carlosWeb from "../public/proyects/carlosWeb.jpg";
+
+// import certificate2 from "../public/certificate2.png";
+// import certificate3 from "../public/certificate3.png";
+// import certificate4 from "../public/certificate4.png";
+// import certificate5 from "../public/certificate5.png";
 
 export default function Poyects() {
   return (
@@ -12,33 +16,73 @@ export default function Poyects() {
       <h2 style={{ color: "white", textAlign: "center" }}>Proyects</h2>
       <div id="containerPro">
         <div id="ContainerProBlock">
-          <a href="https://github.com/carlosM-toro-martinez" target="_blank">
+          <a
+            href="https://play.google.com/store/apps/details?id=com.encuentra&pli=1"
+            target="_blank"
+          >
             <div
               id="containerCertP"
-              style={{ backgroundImage: ` url(${certificate1})` }}
+              style={{ backgroundImage: ` url(${encuentra})` }}
             >
               <div id="shadowP">
                 <p id="paragraphProyects">
-                  hola como estas esta es una prueba para saber si esto
-                  funcionara o no funcionara
+                  <h4>Development Tools</h4>
+                  App Movil, javascript, React Native CLI,
+                  react-navigation/native, mapbox, socket.io, Android.
                 </p>
               </div>
             </div>
           </a>
-          <a href="https://github.com/carlosM-toro-martinez" target="_blank">
+          <a href="https://proyecto-arcane.firebaseapp.com" target="_blank">
             <div
               id="containerCertP"
-              style={{ backgroundImage: ` url(${certificate2})` }}
+              style={{ backgroundImage: ` url(${arcane})` }}
             >
               <div id="shadowP">
                 <p id="paragraphProyects">
-                  hola como estas esta es una prueba para saber si esto
-                  funcionara o no funcionara
+                  <h4>Development Tools</h4>
+                  App Web, html, css, javascript, bulma, firebase
                 </p>
               </div>
             </div>
           </a>
-          <a href="https://github.com/carlosM-toro-martinez" target="_blank">
+
+          <a
+            href="https://transcendent-malabi-fe52e4.netlify.app/"
+            target="_blank"
+          >
+            <div
+              id="containerCertP"
+              style={{ backgroundImage: ` url(${continentes})` }}
+            >
+              <div id="shadowP">
+                <p id="paragraphProyects">
+                  <h4>Development Tools</h4>
+                  App Web, react, create-react-app, styled-components,
+                  react-bootstrap, GraphQl Client, netlify.
+                </p>
+              </div>
+            </div>
+          </a>
+
+          <a
+            href="http://ec2-54-158-220-237.compute-1.amazonaws.com/"
+            target="_blank"
+          >
+            <div
+              id="containerCertP"
+              style={{ backgroundImage: ` url(${carlosWeb})` }}
+            >
+              <div id="shadowP">
+                <p id="paragraphProyects">
+                  <h4>Development Tools</h4>
+                  App Web, react, create-react-app, styled-components,
+                  react-bootstrap, node js, express, AWS
+                </p>
+              </div>
+            </div>
+          </a>
+          {/* <a href="https://github.com/carlosM-toro-martinez" target="_blank">
             <div
               id="containerCertP"
               style={{ backgroundImage: ` url(${certificate3})` }}
@@ -64,7 +108,7 @@ export default function Poyects() {
                 </p>
               </div>
             </div>
-          </a>
+          </a> */}
         </div>
       </div>
     </div>
